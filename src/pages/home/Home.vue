01670cf2525fe7ada3a231d3e4d33b0dfa38435d
lovelycarrot <little_carrot@163.com>
2019-03-31 18:04:13 +0800

    init project

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home">
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '@/pages/home/components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#home {
+    min-height: 100vh;
+    background: $bgLineGradient;
+    padding: 0 30px;
+}
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="header">
+    <span>
+      <i class="iconfont product-icon">&#xe66c;</i>产品
+    </span>
+    <span class="iconfont person-icon">&#xe676;</span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    height: 88px;
+    line-height: 88px;
+    overflow: hidden;
+    display: flex;
+    justify-content: space-between;
+    color: $fontColor;
+    font-size: 32px;
+    font-family: '黑体';
+    .product-icon {
+        margin-right: 10px;
+    }
+    .person-icon {
+        font-size: 40px;
+    }
+}
+</style>
